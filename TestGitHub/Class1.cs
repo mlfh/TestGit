1a1f0548f5585f840a99ee752bb54dcbeb4534a7
liqiangtq <liqiang@tq-soft.com>
2014-02-08 12:34:32 +0800

    Add proj
    Add proj

---
+++ TestGitHub/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitHub
+{
+    public class Class1
+    {
+        private string TestFun1()
+        {
+            return "Hello.";
+        }
+    }
+}
