3de31972059edf3f52dc6d153568e0f8567bccbb
liqiangtq <liqiang@tq-soft.com>
2014-02-10 16:25:13 +0800

    daadd mothod

---
+++ TestGitHub/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGitHub
+{
+    public class Class2
+    {
+        protected string Class2TestFun1()
+        {
+            return string.Empty;
+        }
+    }
+}
